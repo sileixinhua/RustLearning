d66dd7ab8ebe9dba1aa1104b4901ffd43d025330
sileixinhua <sileixinhua@163.com>
2020-04-08 00:52:56 +0800

    add guess stdin in guess_what_game.rs

--- src/guess_what_game.rs
+++ src/guess_what_game.rs
@@ -1,9 +1,14 @@
-
+use std::io;
 
 fn guess_what_game() {
 
     println!("Hello, world! Start guess what game!");
     //此处 println! 是一个Rust 宏（macro），不是一个普通函数
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed is {}", guess);
 
-    
+
 }
--- target/debug/untitled.d
+++ target/debug/untitled.d
@@ -1,1 +1,1 @@
-/Users/silei/Documents/Code/RustLearning/target/debug/untitled.dSYM: /Users/silei/Documents/Code/RustLearning/src/main.rs
+/Users/silei/Documents/Code/RustLearning/target/debug/untitled.dSYM: /Users/silei/Documents/Code/RustLearning/src/guess_what_game.rs /Users/silei/Documents/Code/RustLearning/src/main.rs
